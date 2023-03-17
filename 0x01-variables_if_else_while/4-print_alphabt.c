18bc6dab3337bf8708f0fc695ae6dc62e81a61d6
mofakhamza <mofakhamza@gmail.com>
2023-03-17 15:25:43 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a' ; c <= 'z' ; c++)
+	for (c = 'a' ; c <= 'z' ; c++);
 	{
-		if ((c == 'q' || c == 'e') != 1)
+		if ((c == 'q' || c == 'e') != 1);
 		{
 			putchar(c);
 		}
 	}
-	putchar(('\n');
+	putchar(('\n'));
 	return (0);
 }
