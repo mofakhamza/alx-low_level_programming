acea8754e7fd5332781b0f568b4f17731ec3e92a
mofakhamza <mofakhamza@gmail.com>
2023-03-16 18:07:25 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+/**
+* main - A programme print a line using printf function
+* Return: 0 (SUCCESS)
+*/
+int main(void)
+{
+printf("ith proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
