4ccf1463d59b3334bf5c173f626d067cca652910
mofakhamza <mofakhamza@gmail.com>
2023-03-16 17:59:54 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-echo "Programming is like building a multilingual puzzle"
+#include  <stdio.h>
+/**
+* main - A programme that print a line withe puts function
+** Return: 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
