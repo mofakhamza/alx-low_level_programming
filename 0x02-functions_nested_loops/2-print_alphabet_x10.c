c8dd008ef42fce1de4b31fa6e4ac057f2f4793ce
mofakhamza <mofakhamza@gmail.com>
2023-03-21 17:24:07 -0700

    all in one

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,13 +12,14 @@ void print_alphabet_x10(void)
 	i = 0;
 
 	while (i < 10)
-		ch = 'a';
-	while (ch <= 'z')
 	{
-		_putchar(ch);
-		ch++;
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
 	}
-	_putchar('\n');
-	i++;
-}
 }
